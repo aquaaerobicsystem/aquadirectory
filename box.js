2ec15117bf158c17678d205d0c086433a0a1818d
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2023-02-13 05:47:20 -0600

    Update box.js

--- box.js
+++ box.js
@@ -3,6 +3,7 @@ const params = new URLSearchParams(window.location.href);
 const x1 = params.get("width");
 const y1 = params.get("height");
 const f = params.get("floor");
+const cube = params.get("cubicle");
 //const office = params.get("office");
 var img;
 if (f === "first"){
@@ -25,8 +26,37 @@ grd.addColorStop(0,"blue");
 grd.addColorStop(1,"blue");
 
 // Fill with gradient
+//ctx.fillStyle = grd;
+//ctx.fillRect(x1,y1,25,25);
+    
+if (cube != null)
+{
+    cubicle(cube);
+}else{
+    // Fill with gradient
 ctx.fillStyle = grd;
 ctx.fillRect(x1,y1,25,25);
+    
+}
+    
+function cubicle(n)
+    {
+        switch (n) {
+  case 1:
+   // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(188,23,25,25);
+    break;
+  case 2:
+    text = "On";
+    break;
+  default:
+    ctx.fillStyle = grd;
+    ctx.fillRect(x1,y1,25,25);
+}
+        
+    }
+
 };
 
 
